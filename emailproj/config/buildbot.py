4382ab20c1dee6e846fa1f93b87d31559cb53eaf
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-26 19:10:12 +0200

    mise en prod

---
+++ emailproj/config/buildbot.py
@@ -0,0 +1,12 @@
+debug = False
+
+database = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'emailapi',
+        'USER': 'emailapi',
+        'PASSWORD': 'qaxLUfVy',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
